e3a03f9524ff083a6023ba8cfdad37fe2e4b1195
Walducha1908 <walduchampwb1908@gmail.com>
2019-11-06 15:10:06 +0100

    CinemaMan class fully implemnted

--- MovieModule/src/Main.java
+++ MovieModule/src/Main.java
@@ -1,5 +1,10 @@
+import Module.CinemaMan;
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello movies!");
+        CinemaMan cinemaMan = new CinemaMan(1, "Mateusz", "Walczak", new Date());
+        System.out.println(cinemaMan);
     }
 }

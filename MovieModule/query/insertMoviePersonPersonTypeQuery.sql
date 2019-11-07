6677c297cb829ffbd6e52bf487a73537b8932f52
Walducha1908 <walduchampwb1908@gmail.com>
2019-11-07 11:36:52 +0100

    insert querys added

---
+++ MovieModule/query/insertMoviePersonPersonTypeQuery.sql
@@ -0,0 +1,46 @@
+use cinema;
+
+-- movie person person type table inserts --
+
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (1, 1, 3)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (1, 2, 4)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (1, 2, 7)
+
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (2, 1, 1)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (2, 2, 7)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (2, 2, 9)
+
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (3, 1, 1)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (3, 2, 4)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (3, 2, 5)
+
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (4, 1, 4)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (4, 2, 3)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (4, 2, 1)
+
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (5, 1, 2)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (5, 1, 7)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (5, 2, 3)
+
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (6, 1, 10)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (6, 1, 9)
+
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (7, 1, 8)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (7, 2, 8)
+
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (8, 1, 7)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (8, 2, 8)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (8, 2, 9)
+
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (9, 1, 10)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (9, 1, 3)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (9, 2, 9)
+
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (10, 1, 7)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (10, 2, 10)
+insert into MoviePersonPersonType (MovieId, PersonTypeId, PersonId) values (10, 2, 8)
+
+-- checking results --
+select *from MoviePersonPersonType
+select *from Movie
+select *from Person

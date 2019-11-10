6acd3e955127cc70a95f58a78e0e369490ced41c
KonradK98 <zangetsuxxl2@gmail.com>
2019-11-10 19:31:20 +0100

    first fx aproach

--- MovieModule/src/Main.java
+++ MovieModule/src/Main.java
@@ -7,9 +7,27 @@ import java.time.Duration;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    Button button;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("First working window");
+        primaryStage.show();
+    }
+
 
     public static void main(String[] args) {
+        launch(args);
 
         BaseDB.setHost("localhost");
         BaseDB.setDbname("cinema");

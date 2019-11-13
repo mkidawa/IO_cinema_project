fb6957c31b2b0d583e58bb44ccf5f677e0bb0507
Walducha1908 <walduchampwb1908@gmail.com>
2019-11-13 22:07:04 +0100

    main movie window added

--- Cinema/src/main/java/Program.java
+++ Cinema/src/main/java/Program.java
@@ -1,50 +1,28 @@
-import Controller.MovieManager;
-import DBO.*;
-import Model.DICT.MovieState;
-import Model.DICT.MovieType;
-import Model.DICT.PersonType;
-import Model.Movie;
-import Model.Person;
-import Model.PersonJob;
-import Tools.Filter;
+import View.MovieView.MoviePanel;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import lombok.var;
 
-import javax.sound.midi.SysexMessage;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.time.LocalTime;
-import java.util.LinkedList;
-import java.util.List;
-import javafx.*;
+import java.awt.*;
 
 
-public class Program {
+public class Program extends Application {
     public static void main(String[] args) {
-        //TODO Main program file. System starts here
-        try {
-            test();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+       launch(args);
     }
 
-    private static void test() throws Exception {
-
-//        Filter filter = new Filter();
-//        filter.addField("Id", "3");
-//        List state = MovieStateDAO.getAllByFilter(filter);
-//        System.out.println(state);
-
-        List<MovieType> movieTypes = MovieTypeDAO.getAll();
-        List<MovieState> movieStates = MovieStateDAO.getAll();
-        List<Person> persons = PersonDAO.getAllById(3);
-        List<PersonType> personTypes = PersonTypeDAO.getAll();
-        LinkedList<PersonJob> personJobs = new LinkedList<PersonJob>();
-        personJobs.add(new PersonJob(persons.get(0), personTypes.get(1)));
-        personJobs.add(new PersonJob(persons.get(0), personTypes.get(0)));
-        MovieManager.createMovie((short)1, (short)0, (short)1, movieTypes.get(1), movieStates.get(0),
-                "bella fantastica", "molto bene", Time.valueOf("1:34:00"), personJobs);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent fxmlLoader = FXMLLoader.load(getClass().getResource("/MoviePanel/mainMovie.fxml"));
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Scene scene = new Scene(fxmlLoader);
+        scene.getStylesheets().add(getClass().getResource("/MoviePanel/mainMovie.css").toExternalForm());
+        primaryStage.setTitle("Main Movie Panel");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
 }
---
+++ Cinema/src/main/java/View/MovieView/MoviePanel.java
@@ -0,0 +1,84 @@
+package View.MovieView;
+
+import DBO.MovieDAO;
+import Model.Movie;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.var;
+
+import javax.swing.text.TabableView;
+import java.awt.*;
+import java.util.List;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MoviePanel implements Initializable {
+    @FXML private GridPane pane;
+    @FXML private TableView<SimpleMovie> table;
+    @FXML private TableColumn<SimpleMovie, Long> id;
+    @FXML private TableColumn<SimpleMovie, String> title;
+    @FXML private TableColumn<SimpleMovie, String> description;
+
+    private ObservableList<SimpleMovie> list = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        list = getList();
+        id.setCellValueFactory(new PropertyValueFactory<SimpleMovie, Long>("id"));
+        title.setCellValueFactory(new PropertyValueFactory<SimpleMovie, String>("title"));
+        description.setCellValueFactory(new PropertyValueFactory<SimpleMovie, String>("description"));
+
+        table.setItems(list);
+    }
+
+    public ObservableList<SimpleMovie> getList() {
+        ObservableList<SimpleMovie> list = FXCollections.observableArrayList();
+        List<Movie> movies = MovieDAO.getAll();
+        for (int i=0; i<movies.size(); i++) {
+            list.add(new SimpleMovie(movies.get(i).getId(), movies.get(i).getTitle(), movies.get(i).getDescription()));
+        }
+        return list;
+    }
+
+    public class SimpleMovie {
+        private final SimpleLongProperty id;
+        private final SimpleStringProperty title;
+        private final SimpleStringProperty description;
+
+        public SimpleMovie(long id, String title, String description) {
+            this.id = new SimpleLongProperty(id);
+            this.title = new SimpleStringProperty(title);
+            this.description = new SimpleStringProperty(description);
+        }
+
+        public long getId() {
+            return id.get();
+        }
+
+        public String getTitle() {
+            return title.get();
+        }
+
+        public String getDescription() {
+            return description.get();
+        }
+    }
+}

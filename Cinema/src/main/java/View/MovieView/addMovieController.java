c062cfe662ffb352489919cbc85462d002fbe458
KonradK98 <zangetsuxxl2@gmail.com>
2019-11-16 15:22:10 +0100

    controls

--- Cinema/src/main/java/View/MovieView/addMovieController.java
+++ Cinema/src/main/java/View/MovieView/addMovieController.java
@@ -1,13 +1,31 @@
 package View.MovieView;
 
+import Controller.MovieManager;
+import DBO.MovieDAO;
+import DBO.MovieTypeDAO;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class addMovieController implements Initializable {
+    @FXML private TextField title;
+    @FXML private TextField description;
+    @FXML private ComboBox Genre;
+    @FXML private ComboBox MovieState;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //MovieManager.createMovie();
+        List list = MovieTypeDAO.getAll();
+        Genre.getItems().addAll(list);
+
+
 
     }
 }

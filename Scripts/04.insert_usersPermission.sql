c82663be3ce8de4bb3dace6a3e85caa8db1ea2db
Bartłomiej Jencz <bartek.jencz@wp.pl>
2019-11-21 20:32:44 +0100

    Additional inserts to DB

---
+++ Scripts/04.insert_usersPermission.sql
@@ -0,0 +1,6 @@
+INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (1, 1);
+INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (2, 1);
+INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (2, 2);
+INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (2, 3);
+INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (1, 6);
+INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (4, 1);

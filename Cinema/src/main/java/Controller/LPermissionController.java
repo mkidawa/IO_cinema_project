a7726225e9dbbab8f10b9f3af26f520bd38be46d
Bartłomiej Jencz <Jenczu11@gmail.com>
2019-11-21 19:07:23 +0100

    Added CheckLogin

--- Cinema/src/main/java/Controller/LPermissionController.java
+++ Cinema/src/main/java/Controller/LPermissionController.java
@@ -20,12 +20,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LPermissionController {
+    private User currentUser;
     private static LPermissionController ourInstance;
     public static LPermissionController getInstance() {
         if (ourInstance == null) {
             ourInstance = new LPermissionController();
         }
         return ourInstance;
+    }
+    private LPermissionController() {
+        currentUser=new User();
     }
     public void getAccessLevel(){
 
@@ -40,8 +44,41 @@ public class LPermissionController {
 //        System.out.println(UserDAO.getAllByFilter(fl));
 //        System.out.println(UserDAO.getAll());
 //        Hibernate.initialize(UserDAO.getAll());
-        System.out.println(UserDAO.getAll());
-        return true;
+//        System.out.println(UserDAO.getAllByFilter(fl));
+        String sql = "SELECT U.login from User U WHERE U.login='" + login +
+                "'";
+        List result = UserDAO.execSQL(sql);
+        if(result.size()==0) {
+            System.out.println("Brak uzytkownika w bazie");
+            return false;
+        } else if(result.size()==1 && result.get(0).toString().equals(login))
+        {
+
+            System.out.println("Znalazlem login: "+login);
+
+            return true;
+        }
+        return false;
+    }
+
+    public boolean login(String login, String password) {
+        if (checkLogin(login)) {
+            if (checkPassword(login, password)) {
+                password = "";
+                setAccessType();
+            }
+        }
+        return false;
+    }
+
+    private boolean checkPassword(String login, String password) {
+        String sql = "SELECT U.login, U.passwordHash from User U WHERE U.login='" + login +
+                "'";
+        String username;
+        String passwordFromDataBase;
+    }
+
+    private void setAccessType() {
     }
 
 }

6069d743cb4b4a44cd15d351207d8fcf10992dcc
Bartlomiej Jencz <bartek.jencz@wp.pl>
2019-11-27 12:30:10 +0100

    Fixed scripts [deleted IO]

--- Scripts/InsertPermissions.sql
+++
@@ -1,7 +0,0 @@
-use IO;
-insert into DICTPermission values ('Dodawanie filmow', 1);
-insert into DICTPermission values ('Usuwanie filmow', 2);
-insert into DICTPermission values ('Edytowanie filmow', 3);
-insert into DICTPermission values ('Tworzenie nowych uzytkownikow', 4);
-insert into DICTPermission values ('Usuwanie uzytkownikow', 5);
-insert into DICTPermission values ('Tworzenie raportow', 7);
--- Scripts/InsertUsersPermission.sql
+++
@@ -1,7 +0,0 @@
-use IO;
-insert into UsersPermission values('1','1')
-insert into UsersPermission values('1','2')
-insert into UsersPermission values('2','2')
-insert into UsersPermission values('3','2')
-insert into UsersPermission values('1','1')
-insert into UsersPermission values('1','4')

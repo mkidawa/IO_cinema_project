c378a4acabaa8ca8f0938039fe54a56f90d90e11
Kamil Kowalewski <szamil128@onet.pl>
2019-12-01 19:06:04 +0100

    Add getting data from PerformanceCreator and validation

--- Cinema/src/main/java/View/TimetableModule/PerformanceCreator.java
+++ Cinema/src/main/java/View/TimetableModule/PerformanceCreator.java
@@ -3,12 +3,8 @@ package View.TimetableModule;
 import Controller.StageManager;
 import DBO.HallDAO;
 import DBO.MovieDAO;
-import DBO.PerformanceDAO;
-import Model.DICT.MovieState;
-import Model.DICT.MovieType;
 import Model.Hall;
 import Model.Movie;
-import Model.Performance;
 import View.TimetableModule.Util.PopOutWindow;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -23,7 +19,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Time;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -46,6 +41,12 @@ public class PerformanceCreator implements Initializable {
     private RadioButton flgVR;
     @FXML
     private Button confirmButton;
+
+    private short flag2dValue;
+    private short flag3dValue;
+    private short flagVRValue;
+    private long hallIdValue;
+    private String titleValue;
 
     private PopOutWindow popOutWindow = new PopOutWindow();
 
@@ -84,6 +85,49 @@ public class PerformanceCreator implements Initializable {
         halls.forEach((it) -> comboBoxHallNumber.getItems().add(it.getId()));
     }
 
+    private boolean isOneRadioButtonSelected(RadioButton... radioButtons) {
+        int numberOfSelectedRadioButtons = 0;
+
+        for (RadioButton it : radioButtons) {
+            if (it.isSelected()) {
+                numberOfSelectedRadioButtons++;
+            }
+        }
+
+        if (numberOfSelectedRadioButtons == 1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean areComboBoxesFilled(ComboBox... comboBoxes) {
+        for (ComboBox it : comboBoxes) {
+            if (it.getValue() == null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private short setFlagValue(RadioButton radioButton, short value) {
+        if (radioButton.isSelected()) {
+            value = 1;
+        }
+
+        return value;
+    }
+
+    private void getValueFromInputs() {
+        flag2dValue = setFlagValue(flg2D, flag2dValue);
+        flag3dValue = setFlagValue(flg3D, flag3dValue);
+        flagVRValue = setFlagValue(flgVR, flagVRValue);
+
+        titleValue = (String) comboBoxTitle.getValue();
+        hallIdValue = (Long) comboBoxHallNumber.getValue();
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         fillComboBoxes();
@@ -92,14 +136,24 @@ public class PerformanceCreator implements Initializable {
     @FXML
     private void onClickConfirm(MouseEvent mouseEvent) {
         // TODO REMOVE THIS
-        PerformanceDAO.insertUpdate(new Performance(new Movie((short) 1, (short) 1, (short) 1,
-                new MovieType("Comedy"), new MovieState("vfvfvf"), "WEEIA vs FTIMS",
-                "Only one true win(n)er", new Time(15)),
-                new Hall((short) 1, (short) 0, (short) 0, 5, 7, "acdcd",
-                        "nrinvnvklfdvklfdkvfkvkfn"), new Time(25)));
+//        PerformanceDAO.insertUpdate(new Performance(new Movie((short) 1, (short) 1, (short) 1,
+//                new MovieType("Comedy"), new MovieState("vfvfvf"), "WEEIA vs FTIMS",
+//                "Only one true win(n)er", new Time(15)),
+//                new Hall((short) 1, (short) 0, (short) 0, 5, 7, "acdcd",
+//                        "nrinvnvklfdvklfdkvfkvkfn"), new Time(25)));
 
-        reloadStage(confirmButton, TIMETABLE_PANEL_PATH,
-                TIMETABLE_PANEL_STYLE_PATH, TIMETABLE_PANEL);
+        if (isOneRadioButtonSelected(flg2D, flg3D, flgVR)
+                && areComboBoxesFilled(comboBoxTitle, comboBoxHallNumber)) {
+            getValueFromInputs();
 
+//        TODO HERE CREATE PERFORMANCE OBJECT BASED ON FIELDS IN THIS CLASS AND SAVE TO DB
+
+            reloadStage(confirmButton, TIMETABLE_PANEL_PATH,
+                    TIMETABLE_PANEL_STYLE_PATH, TIMETABLE_PANEL);
+        } else {
+            popOutWindow.messageBox("Empty Input Warning",
+                    "Every input must be selected and exactly one movie type must be selected",
+                    Alert.AlertType.WARNING);
+        }
     }
 }

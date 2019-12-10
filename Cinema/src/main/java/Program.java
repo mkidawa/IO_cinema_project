d4e0c077b906e9bec58b69ba26bf4a7aeba86307
Bartlomiej Jencz <bartek.jencz@wp.pl>
2019-12-10 22:13:00 +0100

    Quick template for MM

--- Cinema/src/main/java/Program.java
+++ Cinema/src/main/java/Program.java
@@ -2,23 +2,35 @@ import Controller.LPermissionController;
 import Tools.BaseDB;
 import Tools.PermissionChecker;
 import View.Login.LoginView;
+import View.Login.MA;
 import javafx.application.Application;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import static java.lang.System.exit;
 
 public class Program extends Application {
     @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Cinema Management System");
 //        var myLoader = new FXMLLoader(getClass().getResource("login/LoginView.fxml"));
-        //Pane myPane = myLoader.load();
+//        Pane myPane = myLoader.load();
 
         //var myScene = new Scene();
-        primaryStage.setScene(new LoginView().getScene());
+        primaryStage.setScene(new MA().getScene());
         primaryStage.show();
         //primaryStage.setFullScreen(true);
 
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
     }
 
     public static void main(String[] args) {

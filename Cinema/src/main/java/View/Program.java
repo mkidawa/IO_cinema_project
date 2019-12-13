33e3fdd296fd3ae56e100a187a46c25255199f5f
Bartlomiej Jencz <bartek.jencz@wp.pl>
2019-12-13 13:23:55 +0100

    Added nicer login/menu

--- Cinema/src/main/java/View/Program.java
+++ Cinema/src/main/java/View/Program.java
@@ -1,24 +1,32 @@
-import Controller.LPermissionController;
-import Tools.BaseDB;
-import Tools.PermissionChecker;
-import View.Login.LoginView;
-import View.MainMenu.MainMenu;
+package View;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import static java.lang.System.exit;
-
 public class Program extends Application {
+    //https://stackoverflow.com/questions/15805881/how-can-i-obtain-the-primary-stage-in-a-javafx-application
+
+    //README !
+    // To pozwala odwolac sie do primaryStage i podmienic okienko (np dla logowania)
+    public static Stage pStage;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainMenu/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/MainMenu/Login.fxml"));
         primaryStage.setTitle("Cinema Management");
         primaryStage.setScene(new Scene(root, 800 , 600));
         primaryStage.show();
+        pStage = primaryStage;
+    }
 
+    public static Stage getPrimaryStage() {
+        return pStage;
+    }
+
+    public static void setPrimaryStage(Stage pStage) {
+        Program.pStage = pStage;
     }
 
     public static void main(String[] args) {

03f5d5a79aa3afeecd0841f438a72ac9251329b3
CaptainSuszkow <lysomice72@gmail.com>
2019-12-17 20:42:10 +0000

    Updated SimpleSale

--- Cinema/src/main/java/View/Sale/Simple/SimpleSale.java
+++ Cinema/src/main/java/View/Sale/Simple/SimpleSale.java
@@ -2,6 +2,7 @@ package View.Sale.Simple;
 
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class SimpleSale {
@@ -9,17 +10,18 @@ public class SimpleSale {
     private SimpleStringProperty packName;
     private SimpleIntegerProperty amount;
     private SimpleDoubleProperty price;
+    private SimpleLongProperty packId;
 
 
-    public SimpleSale(String packName, int amount, Double price) {
+    public SimpleSale(String packName, int amount, Double price, long packId) {
         this.packName = new SimpleStringProperty(packName);
         this.amount = new SimpleIntegerProperty(amount);
         this.price = new SimpleDoubleProperty(price);
+        this.packId = new SimpleLongProperty(packId);
     }
 
     public String getPackName(){return this.packName.get();}
-    public long getAmount(){return this.amount.get();}
+    public int getAmount(){return this.amount.get();}
     public double getPrice(){return this.price.get();}
-
-
+    public long getPackID(){return this.packId.get();}
 }

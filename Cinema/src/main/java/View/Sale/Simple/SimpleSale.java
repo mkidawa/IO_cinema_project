c8f96386840743c2aba64ab1b5af4326843a94b7
CaptainSuszkow <lysomice72@gmail.com>
2019-12-17 22:34:27 +0000

    Fixed SimpleSale

--- Cinema/src/main/java/View/Sale/Simple/SimpleSale.java
+++ Cinema/src/main/java/View/Sale/Simple/SimpleSale.java
@@ -2,6 +2,7 @@ package View.Sale.Simple;
 
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.util.Pair;
 
@@ -9,12 +10,11 @@ import java.util.List;
 
 public class SimpleSale {
 
+    private SimpleLongProperty packHId;
     private SimpleStringProperty packName;
     private SimpleIntegerProperty amount;
     private SimpleDoubleProperty price;
     public List<Pair<Long,Integer>> productInSale;
-
-
 
     public SimpleSale(String packName, int amount, Double price, List<Pair<Long,Integer>> listOfProducts) {
         this.packName = new SimpleStringProperty(packName);
@@ -23,9 +23,18 @@ public class SimpleSale {
         this.productInSale = listOfProducts;
     }
 
+    public SimpleSale(String packName, int amount, Double price, List<Pair<Long,Integer>> listOfProducts, long packHId) {
+        this.packName = new SimpleStringProperty(packName);
+        this.amount = new SimpleIntegerProperty(amount);
+        this.price = new SimpleDoubleProperty(price);
+        this.productInSale = listOfProducts;
+        this.packHId = new SimpleLongProperty(packHId);
+    }
+
     public String getPackName(){return this.packName.get();}
     public long getAmount(){return this.amount.get();}
     public double getPrice(){return this.price.get();}
+    public long getPackHId() { return this.packHId.get();}
 
 
 }

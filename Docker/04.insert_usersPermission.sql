05e17934709208b16d0f1e068936afa59a8d597c
Bartlomiej Jencz <bartek.jencz@wp.pl>
2019-12-17 10:49:16 +0100

    Updated docker

--- Docker/04.insert_usersPermission.sql
+++ Docker/04.insert_usersPermission.sql
@@ -1,6 +1,7 @@
-INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (1, 1);
-INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (2, 1);
-INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (2, 2);
-INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (2, 3);
-INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (1, 6);
-INSERT INTO IO.dbo.UsersPermission (UserId, PermissionId) VALUES (4, 1);
+use IO
+INSERT INTO dbo.UsersPermission (UserId, PermissionId) VALUES (1, 1);
+INSERT INTO dbo.UsersPermission (UserId, PermissionId) VALUES (2, 1);
+INSERT INTO dbo.UsersPermission (UserId, PermissionId) VALUES (2, 2);
+INSERT INTO dbo.UsersPermission (UserId, PermissionId) VALUES (2, 3);
+INSERT INTO dbo.UsersPermission (UserId, PermissionId) VALUES (1, 6);
+INSERT INTO dbo.UsersPermission (UserId, PermissionId) VALUES (4, 1);

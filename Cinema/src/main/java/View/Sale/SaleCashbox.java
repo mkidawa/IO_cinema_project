2cff38a55509f49fba306d4e5d89cbedc41e7a71
Skyblade8715 <piotr8715@gmail.com>
2019-12-17 21:19:25 +0100

    I've done something.

--- Cinema/src/main/java/View/Sale/SaleCashbox.java
+++ Cinema/src/main/java/View/Sale/SaleCashbox.java
@@ -109,9 +109,11 @@ public class SaleCashbox {
             int keyIndex = 0;
             boolean enoughInMagazine = true;
             boolean keyExists = false;
+            List<Pair<Long, Integer>> amountOfProductsInPack = new ArrayList<>();
 
             for (int i = 0; i < packContentList.size(); i++) {
                 keyExists = false;
+                amountOfProductsInPack.add(new Pair<>(packContentList.get(i).getId(), packContentList.get(i).getAmount()));
                 for (int j = 0; j < amountPairs.size(); j++) {
                     if (amountPairs.get(j).getKey() == packContentList.get(i).getId()) {
                         keyIndex = j;
@@ -149,71 +151,39 @@ public class SaleCashbox {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION, "FUCKIN SUCEEDED", ButtonType.CLOSE);
                 alert.showAndWait();
 
-                orderContent.add(new SimpleSale(simplePack.getName(), Integer.valueOf(amount), simplePack.price.multiply(Integer.valueOf(amount)).doubleValue()));
+                orderContent.add(
+                        new SimpleSale(
+                            simplePack.getName(),
+                            Integer.valueOf(amount),
+                            simplePack.price.multiply(Integer.valueOf(amount)).doubleValue(),
+                            amountOfProductsInPack)
+                        );
                 tableOfOrderContent.setItems(orderContent);
                 tableOfOrderContent.refresh();
             }
+            System.out.print("AMOUNT PAIRS: " + amountPairs.toString() + "\n\n\n\n\n");
         }
     }
 //
     public void removePackFromOrder(){
-        int selectedPack = tableOfPack.getSelectionModel().selectedIndexProperty().get();
-        SimplePack simplePack = packs.get(selectedPack);
+        int selectedPack = tableOfOrderContent.getSelectionModel().selectedIndexProperty().get();
+        SimpleSale simpleSale = orderContent.get(selectedPack);
 
-        TextInputDialog d1 = new TextInputDialog();
-        d1.setTitle("How many packs do you wanna add?");
-        d1.setContentText("Enter amount");
-        Optional<String> result = d1.showAndWait();
-        if (result.isPresent()) {
-            String amount = result.get();
-            int keyIndex = 0;
-            boolean enoughInMagazine = true;
-            boolean keyExists = false;
+        for (int i = 0; i < simpleSale.productInSale.size(); i++) {
+            for (int j = 0; j < amountPairs.size(); j++) {
+                if (amountPairs.get(j).getKey().equals(simpleSale.productInSale.get(i).getKey())) {
+                    int newAmount = (int)
+                            (amountPairs.get(j).getValue() -
+                                    (simpleSale.productInSale.get(i).getValue() *
+                                            simpleSale.getAmount()));
 
-            for (int i = 0; i < packContentList.size(); i++) {
-                keyExists = false;
-                for (int j = 0; j < amountPairs.size(); j++) {
-                    if (amountPairs.get(j).getKey() == packContentList.get(i).getId()) {
-                        keyIndex = j;
-                        keyExists = true;
-                        break;
-                    }
-                }
-                if (!keyExists) {
-                    if (PackPoDAO.checkAmount(packContentList.get(i).getId(), packContentList.get(i).getAmount() * Integer.valueOf(amount), 0)) {
-
-                        amountPairs.add(new Pair<>(packContentList.get(i).getId(),
-                                packContentList.get(i).getAmount() * Integer.valueOf(amount)));
-                    } else {
-                        Alert alert = new Alert(Alert.AlertType.ERROR, "There is not enough " + simplePack.getName() + " in magazine!", ButtonType.CLOSE);
-                        alert.showAndWait();
-                        enoughInMagazine = false;
-                        break;
-                    }
-                } else {
-                    if (PackPoDAO.checkAmount(packContentList.get(i).getId(), packContentList.get(i).getAmount() * Integer.valueOf(amount), amountPairs.get(keyIndex).getValue())) {
-
-                        int newAmount = amountPairs.get(keyIndex).getValue() +
-                                (Integer.valueOf(amount) * packContentList.get(i).getAmount());
-                        amountPairs.set(keyIndex, new Pair<>(amountPairs.get(keyIndex).getKey(), newAmount));
-
-                    } else {
-                        Alert alert = new Alert(Alert.AlertType.ERROR, "There is not enough " + simplePack.getName() + " in magazine!", ButtonType.CLOSE);
-                        alert.showAndWait();
-                        enoughInMagazine = false;
-                        break;
-                    }
-                }
-            }
-            if(enoughInMagazine) {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION, "FUCKIN SUCEEDED", ButtonType.CLOSE);
-                alert.showAndWait();
-
-                orderContent.add(new SimpleSale(simplePack.getName(), Integer.valueOf(amount), simplePack.price.multiply(Integer.valueOf(amount)).doubleValue()));
-                tableOfOrderContent.setItems(orderContent);
-                tableOfOrderContent.refresh();
+                    amountPairs.set(j, new Pair<>(amountPairs.get(j).getKey(), newAmount));
+              }
             }
         }
+        System.out.print(amountPairs.toString());
+        orderContent.remove(selectedPack);
+        tableOfOrderContent.refresh();
     }
 
 
--- Cinema/src/main/java/View/Sale/Simple/SimpleSale.java
+++ Cinema/src/main/java/View/Sale/Simple/SimpleSale.java
@@ -3,18 +3,24 @@ package View.Sale.Simple;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.util.Pair;
+
+import java.util.List;
 
 public class SimpleSale {
 
     private SimpleStringProperty packName;
     private SimpleIntegerProperty amount;
     private SimpleDoubleProperty price;
+    public List<Pair<Long,Integer>> productInSale;
 
 
-    public SimpleSale(String packName, int amount, Double price) {
+
+    public SimpleSale(String packName, int amount, Double price, List<Pair<Long,Integer>> listOfProducts) {
         this.packName = new SimpleStringProperty(packName);
         this.amount = new SimpleIntegerProperty(amount);
         this.price = new SimpleDoubleProperty(price);
+        this.productInSale = listOfProducts;
     }
 
     public String getPackName(){return this.packName.get();}

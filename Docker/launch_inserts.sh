f9e0509aa0b15d41566c54d13d7cc7c45888bb03
Bartlomiej Jencz <bartek.jencz@wp.pl>
2019-12-17 10:49:16 +0100

    Updated docker

--- Docker/launch_inserts.sh
+++ Docker/launch_inserts.sh
@@ -1,4 +1,5 @@
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Admin123' -i /opt/mssql-scripts/02.insert_users.sql &&
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Admin123' -i /opt/mssql-scripts/03.insert_dictPermission.sql &&
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Admin123' -i /opt/mssql-scripts/04.insert_usersPermission.sql &&
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Admin123' -i /opt/mssql-scripts/05.MovieModuleInserts.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Admin123' -i /opt/mssql-scripts/05.MovieModuleInserts.sql &&
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Admin123' -i /opt/mssql-scripts/06.insert_HallData.sql

7bfc6e98ecb5ff8e5588bd9bf84d94acc6936484
Kjawo <konjaworski@gmail.com>
2020-01-06 14:03:12 +0100

    Add generateIndividualWorkTimeReport

--- Cinema/src/main/java/Controller/ReportGenerator.java
+++ Cinema/src/main/java/Controller/ReportGenerator.java
@@ -6,6 +6,7 @@ import DBO.SaleDAO;
 import DBO.UserDAO;
 import Model.*;
 import Tools.BaseDB;
+import View.Report.ReportPanel;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -107,6 +108,7 @@ public class ReportGenerator {
     }
 
     public static void generateWorkTimeReport() throws IOException {
+        System.out.println();
 
 
         try {
@@ -192,6 +194,94 @@ public class ReportGenerator {
             e.printStackTrace();
         }
     }
+
+    public static void generateIndividualWorkTimeReport(long userId) throws IOException {
+
+
+        try {
+            Path path = Paths.get(ClassLoader.getSystemResource("Images/logo.png").toURI());
+
+            Document document = new Document();
+//            document.setMargins(1,1, 1,1);
+
+            PdfWriter.getInstance(document, new FileOutputStream("WorkTimeReport.pdf"));
+
+            document.open();
+            Font fontTitle = FontFactory.getFont(FontFactory.COURIER, 32, BaseColor.BLACK);
+            Font fontReportTitle = FontFactory.getFont(FontFactory.COURIER, 28, BaseColor.BLACK);
+            Font fontContent = FontFactory.getFont(FontFactory.COURIER, 14, BaseColor.BLACK);
+
+            Image img = Image.getInstance(path.toAbsolutePath().toString());
+
+            img.scaleToFit(PageSize.A4.getWidth()/5, PageSize.A4.getHeight()/5);
+            float x = (float) ((PageSize.A4.getWidth() - img.getScaledWidth()) * 0.5);
+            float y = (float) (PageSize.A4.getHeight() - img.getScaledHeight() * 1.1);
+            img.setAbsolutePosition(x, y);
+
+
+
+            Chunk title = new Chunk("Cinema management system", fontTitle);
+            Chunk reportTitle = new Chunk("Work Time report", fontReportTitle);
+
+            Paragraph preface = new Paragraph();
+            preface.setAlignment(Element.ALIGN_CENTER);
+
+            preface.add(img);
+            preface.add( Chunk.NEWLINE );
+            preface.add( Chunk.NEWLINE );
+            preface.add( Chunk.NEWLINE );
+            preface.add( Chunk.NEWLINE );
+            preface.add( Chunk.NEWLINE );
+            preface.add( Chunk.NEWLINE );
+            preface.add( Chunk.NEWLINE );
+            preface.add( Chunk.NEWLINE );
+            preface.add(title);
+            preface.add( Chunk.NEWLINE );
+            preface.add( Chunk.NEWLINE );
+            preface.add( Chunk.NEWLINE );
+            preface.add(reportTitle);
+            preface.add( Chunk.NEWLINE );
+            preface.add( Chunk.NEWLINE );
+
+
+            Paragraph content = new Paragraph();
+
+            var so = BaseDB.openConnection();
+            so.beginTransaction();
+            List<TnAData> tnadata = so.createQuery("from TnAData where Id = " + String.valueOf(userId)).list();
+            so.getTransaction().commit();
+            so.close();
+
+
+
+            PdfPTable table = new PdfPTable(4);
+
+            table.addCell("User");
+            table.addCell("Date");
+            table.addCell("Time from");
+            table.addCell("Time to");
+
+
+            List<User> currentUser = null;
+            for(TnAData data : tnadata) {
+                currentUser = UserDAO.getAllById(data.getUserId());
+                table.addCell(String.join(" ", currentUser.get(0).getFirstName(), currentUser.get(0).getLastName()));
+                table.addCell(String.valueOf(data.getDateDay()));
+                table.addCell(String.valueOf(data.getTimeFrom()));
+                table.addCell(String.valueOf(data.getTimeTo()));
+            }
+
+            content.add(table);
+
+            document.add(preface);
+            document.add(content);
+
+            document.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public static void generateIncomesReport() throws IOException {
 
--- Cinema/src/main/java/View/Report/ReportPanel.java
+++ Cinema/src/main/java/View/Report/ReportPanel.java
@@ -1,25 +1,35 @@
 package View.Report;
 
+import DBO.UserDAO;
+import Model.User;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ReportPanel implements Initializable {
+    public DatePicker toDate;
+    public DatePicker fromDate;
     @FXML
     private GridPane pane;
     @FXML
     private ComboBox<String> reportList;
+    @FXML
+    private ComboBox<String> userCombo;
 
     private ObservableList<String> list = FXCollections.observableArrayList();
+    private ObservableList<String> userList = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -27,14 +37,34 @@ public class ReportPanel implements Initializable {
         list = FXCollections.observableArrayList(
                 "All Movies Report",
                "Work time report",
+                "Individual Work time report",
                 "Incomes Report"
         );
 
+
         reportList.getItems().addAll(list);
         reportList.setValue(list.get(0));
+
+        List<User> users = UserDAO.getAll();
+
+        List<String> userIds = new ArrayList<>();
+
+
+
+        for(User user : users) {
+            userIds.add(String.valueOf(user.getId()));
+        }
+
+        userList = FXCollections.observableArrayList(userIds);
+
+        userCombo.getItems().addAll(userList);
+        userCombo.setValue(userIds.get(0));
     }
 
     public void onClickGenerate(MouseEvent mouseEvent) throws IOException {
+        System.out.println(toDate.getValue());
+        System.out.println(fromDate.getValue());
+
 
         if(reportList.getSelectionModel().isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -51,6 +81,9 @@ public class ReportPanel implements Initializable {
             }
             case "Work time report": {
                 Controller.ReportGenerator.generateWorkTimeReport();
+            }
+            case "Individual Work time report": {
+                Controller.ReportGenerator.generateIndividualWorkTimeReport(Long.parseLong(userCombo.getValue()));
             }
             case "Incomes Report": {
                 Controller.ReportGenerator.generateIncomesReport();
